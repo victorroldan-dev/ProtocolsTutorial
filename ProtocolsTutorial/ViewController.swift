426824d2519ce333df6dca001a1584cd62afd67d
Victor <victorroldan.dev@gmail.com>
2021-09-07 23:30:12 -0500

    first commit

--- ProtocolsTutorial/ViewController.swift
+++ ProtocolsTutorial/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stringSelectedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func open2ndScreenPressed(_ sender: Any) {
+        let vc = VC2()
+        vc.delegate = self
+        navigationController?.present(vc, animated: true)
+    }
+    
+    @IBAction func openListPressed(_ sender: Any) {
+        let vc = PostsListViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
 
+extension ViewController : StringProtocol{
+    func stringLeght(_ count: Int) {
+        print("count", count)
+    }
+    
+    func didSelectString(_ string: String) {
+        stringSelectedLabel.text = string
+    }
 }
 
